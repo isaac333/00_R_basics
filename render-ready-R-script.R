710f9449ed78899c487212c774742423509ba35b
Isaac Zhao <joysto@163.com>
2021-09-21 10:38:13 +0800

    WIP

--- render-ready-R-script.R
+++ render-ready-R-script.R
@@ -2,9 +2,7 @@
 #' dataset `VADeaths`.
 ## here is a regular code comment, that will remain as such
 
-#' dataset
-#' Here's some prose
-#' Implement awesome feature; closes #43
+
 summary(VADeaths)
 
 #' Here's some more prose. I can use usual markdown syntax to make things
